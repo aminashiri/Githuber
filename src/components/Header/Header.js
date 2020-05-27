455b7e9a0db53a09a30c42f1e9f20770f3718001
ashiri <persicasoft@gmail.com>
2020-05-28 00:55:58 +0430

    Change Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,8 +5,8 @@ const Header = () => {
     return (
         <header className="header header">
         <nav className="header--nav">
-            <a target="_blank" rel="noopener noreferrer"  href="https://www.exozet.com/" title="Exozet Best 2020 Github Resume Maker">
-                 <img className="logo" src="../img/logo.png" alt="Exozet Best Resume Maker"/>
+            <a target="_blank" rel="noopener noreferrer"  href="https://linkedin.com/in/aminashiri/" title="Githuber Best 2020 Github Resume Maker">
+                 <img className="logo" src="../img/logo.png" alt="Githuber Best Resume Maker"/>
             </a>
         </nav>
             <h1 className="header--title">Git Resum&#233; Hub</h1>
